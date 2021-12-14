78f83b132c3a22fff2bb9a933bf8b35f4d2fc5a4
Nika Bodaveli <nbodaveli@rsm.ge>
2021-12-14 13:40:43 +0400

    Getting Books of an Author (ManyToMany relationship)

--- my-books/Data/Services/AuthorsService.cs
+++ my-books/Data/Services/AuthorsService.cs
@@ -24,5 +24,16 @@ namespace my_books.Data.Services
             _context.SaveChanges();
         }
 
+        public AuthorWithBookVM GetAuthorWithBooks(int authorId)
+        {
+            var _author = _context.Authors.Where(n => n.Id == authorId).Select(n => new AuthorWithBookVM()
+            {
+                FullName = n.FullName,
+                BookTitles = n.Book_Authors.Select(n => n.Book.Title).ToList()
+            }).FirstOrDefault();
+
+            return _author;
+        }
+
     }
 }
